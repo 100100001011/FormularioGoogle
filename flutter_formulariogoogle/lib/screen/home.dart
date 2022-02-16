046088bede990b8cb634ed4991ded76356e6bdcb
CrisAlejo23 <89047686+CrisAlejo23@users.noreply.github.com>
2022-02-15 22:12:20 -0500

    fondo

--- flutter_formulariogoogle/lib/screen/home.dart
+++ flutter_formulariogoogle/lib/screen/home.dart
@@ -11,21 +11,34 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Login Page")),
       body: Stack(
         fit: StackFit.expand,
         children: [
+          Positioned(
+              bottom: 582,
+              right: 0,
+              child: Container(
+                width: 395.0,
+                height: 200.0,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                        image: NetworkImage(
+                            'https://cdn.donmai.us/original/58/ca/__ikuuru_and_shaian_aic_and_1_more_drawn_by_kobayashi_makoto_illustrator__58caf5977eb819100fff43e5ab683bd6.jpg'),)),
+              )),
           Positioned(
               bottom: 150,
               right: 20,
               child: Container(
                 margin: const EdgeInsets.only(top: 430, left: 300),
-                color: Colors.amber[600],
                 width: 350.0,
                 height: 400.0,
+                decoration: BoxDecoration(
+                    color: Colors.black12,
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(15)),
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
-                  
                   children: [
                     Padding(
                       padding: const EdgeInsets.only(top: 10.0),
@@ -34,8 +47,8 @@ class _LoginState extends State<Login> {
                             width: 200,
                             height: 150,
                             decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(50.0)),
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(50.0)),
                             child:
                                 Image.asset('asset/images/flutter-logo.png')),
                       ),
@@ -45,12 +58,26 @@ class _LoginState extends State<Login> {
                       padding: EdgeInsets.only(top: 10.0),
                       child: TextField(
                         decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.mail),
+                            prefixIcon: Icon(Icons.mail),
                             border: OutlineInputBorder(),
-                            labelText: 'Correo',
-                            hintText: 'Ingrese el corre@'),
+                            labelText: 'Email',
+                            hintText: 'Ingrese correo'),
                       ),
                     ),
+                    const Padding(
+                      //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+                      padding: EdgeInsets.only(top: 10.0),
+                      child: TextField(
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.lock),
+                            border: OutlineInputBorder(),
+                            labelText: 'Password',
+                            hintText: 'Ingrese el passsword'),
+                      ),
+                    )
                   ],
                 ),
               )),
