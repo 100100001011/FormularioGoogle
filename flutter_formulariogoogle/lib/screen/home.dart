c1192f7253373411b0d4b52a7539d655c2969baa
CrisAlejo23 <89047686+CrisAlejo23@users.noreply.github.com>
2022-02-17 18:41:42 -0500

    imagen

--- flutter_formulariogoogle/lib/screen/home.dart
+++ flutter_formulariogoogle/lib/screen/home.dart
@@ -50,7 +50,7 @@ class _LoginState extends State<Login> {
                                 color: Colors.red,
                                 borderRadius: BorderRadius.circular(50.0)),
                             child:
-                                Image.asset('asset/images/flutter-logo.png')),
+                                Image.asset('Media/Img/1.jpg')),
                       ),
                     ),
                     const Padding(
